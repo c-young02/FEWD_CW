da8cf8f046489c90f43d161b82d5fe7c30f7344c
c-young02 <callum.young02@hotmail.com>
2023-12-23 21:06:05 +0000

    Removed npm install due to changed package location

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to the frontend directory and start the frontend
-(cd frontend && npm install && npm start) &
+(cd frontend && npm start) &
 
 # Navigate to the backend directory and start the backend
-(cd backend && npm install && node index)
+(cd backend && node index)
